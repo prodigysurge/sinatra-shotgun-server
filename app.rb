b4ed00f710c03e500428431aff681a48441f512b
prodigysurge <boatinglife6@yahoo.com>
2021-02-19 11:36:28 -0500

    commit tree 7d0b7efe155e295f7b11326230cf19fa4d7fe48c

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    <!doctype html>
+    <head></head>
+    <body><title>What am I dong?</title><body>
   end
 
 end
